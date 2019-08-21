2b1c04f32db0a37d213b4a46ea5529fa9c43236a
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-21 15:44:31 +0530

    create variable:(userName, String) for accepting user input

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Question1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-
+		String userName = input.nextLine().trim();
 		input.close();
 	}
 	// TODO accept a String value from the user
