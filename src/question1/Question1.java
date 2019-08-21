c5393f8c9e2d481f4e117b591f469924d89962c0
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-21 15:37:46 +0530

    add method:main() for class:Question1

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,4 +1,10 @@
 package question1;
 
 public class Question1 {
+	public static void main(String[] args) {
+
+	}
+	// TODO accept a String value from the user
+
+	// TODO print the welcome message
 }
